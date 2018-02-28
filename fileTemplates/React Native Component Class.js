6b558183d38cb3beb15a58b2d29acb722881dcb2
Pierre <venux92@gmail.com>
2018-02-28 22:06:23 +1100

    PS-2017.2.4 <pierre@Pierres-MacBook-Pro-2.local Create baseRefactoring.xml, markdown.xml, React Native Component Functional༏ext༏..js, IntelliLang.xml, ide.general.xml, project.default.xml, Ahmed-ReconEye_0.icls, diff.xml, Default copy.xml, git.xml, other.xml, Default.xml, Zeus_0.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, colors.scheme.xml, LinArcX_1.icls, databaseDrivers.xml, github_settings.xml, emmet.xml, Default.xml, React-Native.js, laf.xml, web-browsers.xml, _@user_Railscasts.icls, Netbeans.xml, editor.codeinsight.xml, React Native Component Class.js, databaseSettings.xml, keymap.xml, editor.xml, webServers.xml, debugger.xml, PHP File.php

---
+++ fileTemplates/React Native Component Class.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+export default class $File_name extends Component {
+	render() {
+		return (
+			<View></View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ fileTemplates/React Native Component Functional༏ext༏..js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const $name = () => {
+	return (
+		<View>
+			
+		</View>
+	);
+};
+
+export default $name;
+
+const styles = StyleSheet.create({});
